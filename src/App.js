18aa545ee45de760858459cfb26d2e7fcd68dee4
dali010 <mohamedali.hmida@isimg.tn>
2024-11-05 00:08:44 +0100

    update text

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          welcome to my memories application !!!
+          welcome to my memories deployment
         </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
